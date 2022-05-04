d424e478a104d91a9ce524d2435e4934547d14a2
arslanahmadvirk <arslanahmadv098@gmail.com>
2022-05-05 00:10:09 +0500

    update

--- pages/ourServices.js
+++ pages/ourServices.js
@@ -24,74 +24,74 @@ function OurServices() {
           </CardTitle>
           <div className="row">
           <div className="row mt-2 text-center text-dark">
-                <div className="col-lg-4">
+                <div className="col-md-4">
                     <div className="servicesicon">
                     <Image src={truckIcon} className="servicesiconImg mt-4" alt="icon" width={100} height={100}/>
                     </div>
                     <div >
                         <h5 className="font-weight-bold mt-4"> Cargo Shipment
                         </h5>
-                        <p className="text-muted">We are providing full cargo shipment from different cities of Pakistan. You can easily book
+                        <p className="text-muted servicesPara">We are providing full cargo shipment from different cities of Pakistan. You can easily book
                         different types of trucks for your cargo shipment.
                         </p>
                     </div>
                 </div>
-                <div className="col-lg-4 ">
+                <div className="col-md-4 ">
                     <div className="servicesicon ">
                         <Image src={carIcon} className="servicesiconImg mt-4" alt="icon" width={100} height={100}/>
                     </div>
                     <div>
                         <h5 className="font-weight-bold mt-4"> Book Vehicles
                         </h5>
-                        <p className="text-muted"> All kind of vehicles for you. Book all kind of vehicles like buses, cars and vans at offordable rates for your
+                        <p className="text-muted servicesPara"> All kind of vehicles for you. Book all kind of vehicles like buses, cars and vans at offordable rates for your
                         personal uses.
                         </p>
                     </div>
                 </div>
-                <div className="col-lg-4">
+                <div className="col-md-4">
                     <div className="servicesicon">
                     <Image src={driverIcon} className="servicesiconImg mt-4" alt="icon" width={100} height={100}/>
                     </div>
                     <div >
                         <h5 className="font-weight-bold mt-4">Book Drivers
                         </h5>
-                        <p className="text-muted">If you have vehicle and need a driver we make it easy for you.
+                        <p className="text-muted servicesPara">If you have vehicle and need a driver we make it easy for you.
                         Book any type of driver easily in just few minutes.
                         </p>
                     </div>
                 </div>
-                <div className="col-lg-4">
+                <div className="col-md-4">
                     <div className="servicesicon">
                     <Image src={registerIcon} className="servicesiconImg mt-2" alt="icon" width={80} height={80}/>
                     </div>
                     <div >
                         <h5 className="font-weight-bold mt-4"> Register Your Vehicles
                         </h5>
-                        <p className="text-muted"> If you are a transporter and have vehicles, just create a transporter account and
+                        <p className="text-muted servicesPara"> If you are a transporter and have vehicles, just create a transporter account and
                         register your vehicles so customers can book your vehicle.
                         </p>
                     </div>
                 </div>
-                <div className="col-lg-4">
+                <div className="col-md-4">
                     <div className="servicesicon">
                     <Image src={profileIcon} className="servicesiconImg mt-2" alt="icon" width={80} height={80}/>
                     </div>
                     <div >
                         <h5 className="font-weight-bold mt-4"> Register Driver Profile
                         </h5>
-                        <p className="text-muted"> If you are a driver and wants work, just create your driver profile and register yourself
+                        <p className="text-muted servicesPara"> If you are a driver and wants work, just create your driver profile and register yourself
                         so that customers can book you.
                         </p>
                     </div>
                 </div>
-                <div className="col-lg-4">
+                <div className="col-md-4">
                     <div className="servicesicon">
                     <Image src={dashboardIcon} className="servicesiconImg mt-4" alt="icon" width={80} height={80}/>
                     </div>
                     <div >
                         <h5 className="font-weight-bold mt-4">Dashboard
                         </h5>
-                        <p className="text-muted"> Every user will get his own dashboard where he can track his booking resquests easily.
+                        <p className="text-muted servicesPara"> Every user will get his own dashboard where he can track his booking resquests easily.
                         Add or delete your vehicles.
                         </p>
                     </div>
